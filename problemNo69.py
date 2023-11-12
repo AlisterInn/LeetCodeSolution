24d2a353da3fce1c0d49b0d3ef3cae1c07313e18
Jyoti Singh Rojariya <115019676+AlisterInn@users.noreply.github.com>
2023-11-13 01:06:32 +0530

    Update problemNo69.py

--- problemNo69.py
+++ problemNo69.py
@@ -6,5 +6,5 @@ class Solution(object):
         """
         return int(x**0.5)
     
-    """They say that this is not valid but I just started to work in pyhton so I think this is teh only way if someone else have any other solution.
-    Please let me know, of how to do this in more of a abstract way. I posting this only because the leetcode passed it."""
+    """They say that this is not valid but I just started to work in pyhton so I think this is the only way, if someone else have any other solution.
+    Please let me know, of how to do this in more of a abstract way. I'm posting this only because the leetcode passed it."""
