4d0f726219232938904624e1aa2f7a4eb38ba974
Jyoti Singh Rojariya <115019676+AlisterInn@users.noreply.github.com>
2023-11-13 01:05:42 +0530

    Add files via upload

---
+++ problemNo69.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        return int(x**0.5)
+    
+    """They say that this is not valid but I just started to work in pyhton so I think this is teh only way if someone else have any other solution.
+    Please let me know, of how to do this in more of a abstract way. I posting this only because the leetcode passed it."""
