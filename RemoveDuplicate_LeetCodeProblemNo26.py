f99a4e980cced0d8b6e1d0663fff36e5204250cf
Jyoti Singh Rojariya <115019676+AlisterInn@users.noreply.github.com>
2023-11-13 00:41:17 +0530

    Add files via upload

---
+++ RemoveDuplicate_LeetCodeProblemNo26.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) < 2:
+            return  nums
+        else:
+            provious_num = 0
+            for current_num in range(1, len(nums)):
+                if nums[current_num] != nums[provious_num]:
+                    provious_num += 1
+                    nums[provious_num] = nums[current_num]
+            return provious_num+1
